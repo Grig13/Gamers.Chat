272795122494830ff60229138ff7154eaa1e3823
Grig13 <grig.mazureanu@gmail.com>
2022-12-08 20:28:21 +0200

    finished timeline service

--- GamerssChat/Services/TimelineService.cs
+++ GamerssChat/Services/TimelineService.cs
@@ -1,6 +1,51 @@
-﻿namespace GamerssChat.Services
+﻿using GamerssChat.Models;
+using GamerssChat.Repositories.Interfaces;
+
+namespace GamerssChat.Services
 {
     public class TimelineService
     {
+
+        private readonly ITimelineRepository _timelineRepository;
+
+        public TimelineService(ITimelineRepository timelineRepository)
+        {
+            _timelineRepository = timelineRepository;
+        }
+
+        public IEnumerable<Timeline> GetTimelines()
+        {
+            return _timelineRepository.GetAll();
+        }
+
+        public Timeline AddPostsToTimeline(Guid timelineId, List<Post> posts)
+        {
+            return _timelineRepository.AddPostsToTimeline(timelineId, posts);
+        }
+
+        public Timeline AddPostToTimeline(Guid timelineId, Post post)
+        {
+            return _timelineRepository.AddPostToTimeline(timelineId, post);
+        }
+
+        public void DeleteTimeline(Guid id)
+        {
+             this._timelineRepository.DeleteById(id);
+        }
+
+        public Timeline GetTimelineById(Guid id)
+        {
+            return _timelineRepository.GetById(id);
+        }
+
+        public Timeline RemovePostFromTimeline(Guid timelineId, Post postToDelete)
+        {
+            return _timelineRepository.RemovePostFromTimeline(timelineId, postToDelete);
+        }
+
+        public Timeline TimelineToUpdate(Timeline timelineToUpdate)
+        {
+            return _timelineRepository.Update(timelineToUpdate);
+        }
     }
 }
