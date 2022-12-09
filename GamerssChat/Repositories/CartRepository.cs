a616e3e93ef5c918310eb9173291b5dc56551652
Grig13 <grig.mazureanu@gmail.com>
2022-12-09 13:50:37 +0200

    implemented interface repository for cart and started repository for cart.

---
+++ GamerssChat/Repositories/CartRepository.cs
@@ -0,0 +1,43 @@
+﻿using GamerssChat.Models;
+using GamerssChat.Repositories.Interfaces;
+
+namespace GamerssChat.Repositories
+{
+    public class CartRepository : ICartRepository
+    {
+        public Cart AddProductsToCart(Guid cartId, List<Product> products)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cart AddProductToCart(Guid cartId, Product productToAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Cart> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cart GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cart RemoveProductFromCart(Guid cartId, Product productToRemove)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cart Update(Cart cartToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GamerssChat/Repositories/Interfaces/ICartRepository.cs
@@ -0,0 +1,16 @@
+﻿using GamerssChat.Models;
+
+namespace GamerssChat.Repositories.Interfaces
+{
+    public interface ICartRepository
+    {
+        public IEnumerable<Cart> GetAll();
+        public Cart AddProductsToCart(Guid cartId, List<Product> products);
+        public Cart AddProductToCart(Guid cartId, Product productToAdd);
+        public Cart RemoveProductFromCart(Guid cartId, Product productToRemove);
+        public void DeleteById(Guid id);
+        public Cart Update(Cart cartToUpdate);
+        public Cart GetById(Guid id);
+
+    }
+}
